2cbb6f3f5f27493571907c9c01897bf69c61ea6d
Dmitry Latin <dim0xff@gmail.com>
2024-06-08 19:19:25 +0300

    feat: add Unpack

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,13 +2,68 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	output := strings.Builder{}
+
+	// startReadState indicates there are no symbol read yet,
+	// and next read is the starting one.
+	const startReadState rune = -1
+
+	symToUnpack := startReadState // symToUnpack holds symbol to be unpacked.
+	dgt := int(-1)                // -1 means current symbol is not a digit.
+
+	// Store str length to check the last symbol.
+	strLen := len(str)
+
+	// For each symbol in str.
+	for i, sym := range str {
+		// symbol is ASCII: possible digit
+		if sym < 256 {
+			dgt = digit(byte(sym))
+		}
+
+		if dgt == -1 {
+			// Current symbol is not a digit (so no need to repeat previous one symbol).
+			// So if symToUnpack is not in startReadState, write it to output.
+			// Then make current symbol to be next symbol to unpack.
+			if symToUnpack != startReadState {
+				output.WriteRune(symToUnpack)
+			}
+			symToUnpack = sym
+
+			// Edge-case: current symbol is the last symbol in str: write it to output.
+			symLen := utf8.RuneLen(sym)
+			if i+symLen == strLen {
+				output.WriteRune(sym)
+			}
+
+			continue
+		}
+
+		// Current symbol is a digit (so need to repeat previous one symbol).
+
+		// If symToUnpack is in startReadState, return error.
+		if symToUnpack == startReadState {
+			return "", ErrInvalidString
+		}
+
+		// Okay, unpack symbol to output
+		for range dgt {
+			output.WriteRune(symToUnpack)
+		}
+
+		// Reset reads.
+		symToUnpack = startReadState
+		dgt = -1
+	}
+
+	return output.String(), nil
 }
 
 // digit returns int digit (0-9) from provided  ASCII char c.
