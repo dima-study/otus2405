68a65452ceac7b82ed6ed3d88af1a06f444841c7
Dmitry Latin <dim0xff@gmail.com>
2024-06-08 23:27:24 +0300

    feat: Unpack could unpack escaped digit and backslash

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -3,7 +3,6 @@ package hw02unpackstring
 import (
 	"errors"
 	"strings"
-	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
@@ -15,40 +14,71 @@ func Unpack(str string) (string, error) {
 	// and next read is the starting one.
 	const startReadState rune = -1
 
-	symToUnpack := startReadState // symToUnpack holds symbol to be unpacked.
+	// escaped indicates that symToUnpack should be escaped.
+	escaped := false
+
+	// nextToEscape indicates that next symbol should be escaped:
+	// when true, escaped flag will be set to true on next symbol read.
+	nextToEscape := false
+
+	symToUnpack := startReadState // symToUnpack holds symbol to be unpacked (symbol at previous position).
 	dgt := int(-1)                // -1 means current symbol is not a digit.
 
-	// Store str length to check the last symbol.
-	strLen := len(str)
+	// For each symbol sym in str.
+	var sym rune
+	for _, sym = range str {
+		if nextToEscape {
+			escaped = true
+			nextToEscape = false
+		}
 
-	// For each symbol in str.
-	for i, sym := range str {
-		// symbol is ASCII: possible digit
+		// Symbol is ASCII: possible escape or digit.
 		if sym < 256 {
-			dgt = digit(byte(sym))
+			if sym == '\\' {
+				nextToEscape = true
+			} else {
+				dgt = digit(byte(sym))
+			}
+		}
+
+		// Current symbol sym should be escaped.
+		if escaped {
+			if dgt != -1 {
+				// It is escaped digit.
+				symToUnpack = sym
+				dgt = -1
+			} else if sym == '\\' {
+				// It is escaped backslash.
+				symToUnpack = sym
+			} else {
+				// Only digit or backslash(\) could be escaped.
+				return "", ErrInvalidString
+			}
+
+			// Now escaped and ready-to-unpack symbol is in symToUnpack.
+			// Reset escape flags and do next read.
+			escaped = false
+			nextToEscape = false
+			continue
 		}
 
 		if dgt == -1 {
-			// Current symbol is not a digit (so no need to repeat previous one symbol).
+			// CASE: current symbol is not a digit (so no need to repeat previous one symbol).
 			// So if symToUnpack is not in startReadState, write it to output.
-			// Then make current symbol to be next symbol to unpack.
 			if symToUnpack != startReadState {
 				output.WriteRune(symToUnpack)
 			}
-			symToUnpack = sym
 
-			// Edge-case: current symbol is the last symbol in str: write it to output.
-			symLen := utf8.RuneLen(sym)
-			if i+symLen == strLen {
-				output.WriteRune(sym)
-			}
+			// Then make current symbol to be next symbol to unpack.
+			symToUnpack = sym
 
 			continue
 		}
 
-		// Current symbol is a digit (so need to repeat previous one symbol).
+		// CASE: current symbol is a digit (so need to repeat previous one symbol).
 
-		// If symToUnpack is in startReadState, return error.
+		// If symToUnpack is in startReadState: digit has been read, but no previous symbol read.
+		// Return error.
 		if symToUnpack == startReadState {
 			return "", ErrInvalidString
 		}
@@ -61,6 +91,16 @@ func Unpack(str string) (string, error) {
 		// Reset reads.
 		symToUnpack = startReadState
 		dgt = -1
+	}
+
+	// Edge-case: current non-written symbol is the last symbol in str and it is not a digit:
+	//   - write it to output when next should not be escaped;
+	//   - otherwise return error.
+	if symToUnpack != startReadState {
+		if nextToEscape {
+			return "", ErrInvalidString
+		}
+		output.WriteRune(sym)
 	}
 
 	return output.String(), nil
