e79d6e4ac87f5da108cac32ad21155c70c02c34e
Dmitry Latin <dim0xff@gmail.com>
2024-06-12 00:29:50 +0300

    feat: Top10 impl

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,36 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"cmp"
+	"slices"
+	"strings"
+)
+
+// Top10 returns up to 10 words from string s sorted by using frequency + lexicographic order.
+func Top10(s string) []string {
+	// words holds all words from the input string
+	words := strings.Fields(s)
+
+	// wordFreq (word:freq) holds frequency for each unique word.
+	wordFreq := map[string]int{}
+	for _, w := range words {
+		wordFreq[w]++
+	}
+
+	uniqWords := make([]string, 0, len(wordFreq))
+	for w := range wordFreq {
+		uniqWords = append(uniqWords, w)
+	}
+
+	slices.SortFunc(uniqWords, func(a string, b string) int {
+		// 1. sort by frequency
+		if n := cmp.Compare(wordFreq[b], wordFreq[a]); n != 0 {
+			return n
+		}
+
+		// 2. sort in lexicographic order
+		return cmp.Compare(a, b)
+	})
+
+	return uniqWords[:min(10, len(uniqWords))]
 }
