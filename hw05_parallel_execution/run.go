234fb82e921bb7da72526405339cc14309263508
Dmitry Latin <dim0xff@gmail.com>
2024-06-30 15:23:24 +0300

    feat: add run & tests

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,15 +1,85 @@
 package hw05parallelexecution
 
 import (
+	"context"
 	"errors"
+	"sync"
+
+	"github.com/dima-study/otus2405/hw05_parallel_execution/myworkerpool"
+	"github.com/dima-study/otus2405/hw05_parallel_execution/workerpool"
 )
 
-var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var (
+	ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+	ErrWorkerJobCanceled   = errors.New("job has been canceled")
+)
 
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
+// Run ignores tasks errors if m <= 0.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	workerPool := myworkerpool.New()
+
+	// ctx is context to manage available workers.
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Stop all workers at the end.
+	defer cancel()
+
+	// Start n workers.
+	for range n {
+		workerPool.AddWorker(ctx)
+	}
+
+	// wg is used to wait for all tasks to complete.
+	wg := sync.WaitGroup{}
+	wg.Add(len(tasks))
+
+	// errorsCh is used to count the number of occurred errors.
+	errorsCh := make(chan error)
+	for _, task := range tasks {
+		// Create a new job for each task.
+		// Job sends occurred error to the errorsCh channel:
+		//   - ErrWorkerJobCanceled once job is canceled by context
+		//   - error (or nil) returned from task execution
+		var job workerpool.WorkerJob = func(ctx context.Context) {
+			defer wg.Done()
+
+			select {
+			case <-ctx.Done():
+				errorsCh <- ErrWorkerJobCanceled
+				return
+			default:
+			}
+
+			errorsCh <- task()
+		}
+
+		workerPool.AddJob(job)
+	}
+
+	go func() {
+		// Wait until all jobs done and close the errorsCh channel.
+		wg.Wait()
+		close(errorsCh)
+	}()
+
+	// occurredErrors contains the number of occurred errors.
+	occurredErrors := 0
+	for err := range errorsCh {
+		if err != nil {
+			// Cancel all jobs and stop all workers when errors limit is reached.
+			occurredErrors++
+			if occurredErrors == m {
+				cancel()
+			}
+		}
+	}
+
+	if m > 0 && occurredErrors >= m {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -35,7 +35,12 @@ func TestRun(t *testing.T) {
 		err := Run(tasks, workersCount, maxErrorsCount)
 
 		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
-		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
+		require.LessOrEqual(
+			t,
+			runTasksCount,
+			int32(workersCount+maxErrorsCount),
+			"extra tasks were started",
+		)
 	})
 
 	t.Run("tasks without errors", func(t *testing.T) {
@@ -66,5 +71,55 @@ func TestRun(t *testing.T) {
 
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+
+	t.Run("ignore errors", func(t *testing.T) {
+		tests := []struct {
+			name       string
+			workersNum int
+			tasksNum   int
+			errorsNum  int
+		}{
+			{
+				name:       "zero errors",
+				workersNum: 100,
+				tasksNum:   10,
+				errorsNum:  0,
+			},
+			{
+				name:       "with errors",
+				workersNum: 100,
+				tasksNum:   10,
+				errorsNum:  5,
+			},
+		}
+
+		for _, ts := range tests {
+			tasks := make([]Task, ts.tasksNum)
+			runTasksCount := int32(0)
+			errorsNum := int32(ts.errorsNum)
+			for i := range ts.tasksNum {
+				taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+
+				tasks[i] = func() error {
+					time.Sleep(taskSleep)
+					atomic.AddInt32(&runTasksCount, 1)
+
+					if n := atomic.LoadInt32(&errorsNum); n > 0 {
+						atomic.AddInt32(&errorsNum, -1)
+						return fmt.Errorf("error %d for task %d", ts.errorsNum-int(n), i)
+					}
+
+					return nil
+				}
+			}
+
+			t.Run(ts.name, func(t *testing.T) {
+				err := Run(tasks, ts.workersNum, 0)
+				require.NoError(t, err)
+
+				require.Equal(t, runTasksCount, int32(ts.tasksNum), "all tasks must be completed")
+			})
+		}
 	})
 }
