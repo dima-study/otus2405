326e170838c7427caa566e99d713ac9259124779
Dmitry Latin <dim0xff@gmail.com>
2024-07-07 23:29:29 +0300

    feat: add Stage.WithDone

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,60 @@ type (
 
 type Stage func(in In) (out Out)
 
+// WithDone wraps current stage and returns wrapped one, which could be canceled.
+// Close the done channel to cancel stage flow.
+func (stage Stage) WithDone(done In) Stage {
+	return func(in In) Out {
+		// wrappedIn is a new input channel for original stage.
+		// It will be closed once done is closed.
+		wrappedIn := make(Bi)
+
+		go func() {
+			defer close(wrappedIn)
+
+			// Read from original input while possible.
+			for {
+				// Priority cancel.
+				select {
+				case <-done:
+					return
+				default:
+				}
+
+				// Cancel or read from original input.
+				select {
+				case <-done:
+					return
+				case val, ok := <-in:
+					// Original input also could be closed: catch it!
+					if !ok {
+						return
+					}
+
+					// Cancel or write.
+					select {
+					case <-done:
+						return
+					case wrappedIn <- val:
+					}
+				}
+			}
+		}()
+
+		// Make the original stage work with wrapped input.
+		return stage(wrappedIn)
+	}
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		return nil
+	}
+
+	pipeline := stages[0].WithDone(done)(in)
+	for _, stage := range stages[1:] {
+		pipeline = stage.WithDone(done)(pipeline)
+	}
+
+	return pipeline
 }
