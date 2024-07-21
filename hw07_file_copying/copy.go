5aadc797bba28f97582ab7f83d170dbdbd6b0aac
Dmitry Latin <dim0xff@gmail.com>
2024-07-21 22:09:12 +0300

    feat: add progress bar

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+
+	pb "github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -14,8 +16,6 @@ var (
 
 // Copy tries to copy up to limit bytes from fromPath (with offset) into toPath.
 // Returns any occurred error.
-//
-// Expects non-negative offset and limit.
 //
 // Expects non-negative offset and limit.
 func Copy(fromPath, toPath string, offset, limit int64) error {
@@ -61,11 +61,32 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	// fileFromLimited could read up to limit from fileFrom.
 	fileFromLimited := io.LimitReader(fileFrom, limit)
 
-	// Copy with limit or return the error.
-	_, err = io.Copy(fileTo, fileFromLimited)
+	// Create a progress bar with limit.
+	barLimit := progressBarLimit(fInfo.Size(), limit, offset)
+	bar := pb.Full.Start64(barLimit)
+	defer func() {
+		bar.SetCurrent(barLimit)
+		bar.Finish()
+	}()
+
+	// barReader is a proxy reader to calculate progress.
+	barReader := bar.NewProxyReader(fileFromLimited)
+
+	// Copy with limit via progress bar proxy or return the error.
+	_, err = io.Copy(fileTo, barReader)
 	if err != nil {
 		return fmt.Errorf("error while copying file: %w", err)
 	}
 
 	return nil
+}
+
+// progressBarLimit returns limit for progress bar.
+func progressBarLimit(filesize int64, limit int64, offset int64) int64 {
+	barLimit := min(limit, filesize-offset)
+	if filesize == 0 {
+		barLimit = limit
+	}
+
+	return barLimit
 }
