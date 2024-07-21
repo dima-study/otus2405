c2891188fc7d1863511d1884f203efbcd3543519
Dmitry Latin <dim0xff@gmail.com>
2024-07-21 21:34:38 +0300

    feat: implement cli

--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
 var (
@@ -10,13 +12,39 @@ var (
 )
 
 func init() {
-	flag.StringVar(&from, "from", "", "file to read from")
-	flag.StringVar(&to, "to", "", "file to write to")
-	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy")
-	flag.Int64Var(&offset, "offset", 0, "offset in input file")
+	flag.StringVar(&from, "from", "", "file to read from *required*")
+	flag.StringVar(&to, "to", "", "file to write to *required*")
+	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy *>=0*")
+	flag.Int64Var(&offset, "offset", 0, "offset in input file *>=0*")
 }
 
 func main() {
 	flag.Parse()
-	// Place your code here.
+
+	switch {
+	case from == "":
+		flag.Usage()
+		fmt.Println("\nInvalid arguments: 'from' is required")
+		os.Exit(1)
+	case to == "":
+		flag.Usage()
+		fmt.Println("\nInvalid arguments: 'to' is required")
+		os.Exit(1)
+	case offset < 0:
+		flag.Usage()
+		fmt.Println("\nInvalid arguments: 'offset' must be >=0")
+		os.Exit(1)
+	case limit < 0:
+		flag.Usage()
+		fmt.Println("\nInvalid arguments: 'limit' must be >=0")
+		os.Exit(1)
+	}
+
+	err := Copy(from, to, offset, limit)
+	if err != nil {
+		fmt.Printf("Error occurred: %s\n", err)
+		os.Exit(2)
+	}
+
+	fmt.Println("Success!")
 }
