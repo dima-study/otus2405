206586cbdf3e0eec4da162701fd8fe5388eebb26
Dmitry Latin <dim0xff@gmail.com>
2024-07-21 21:04:50 +0300

    refactor: move to switch instead of if

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -11,17 +11,17 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+// Copy tries to copy up to limit bytes from fromPath (with offset) into toPath.
+// Returns any occurred error.
 func Copy(fromPath, toPath string, offset, limit int64) error {
+	// Quick-check if file (not dir!) exists and offset is not exceeds file size.
 	fInfo, err := os.Stat(fromPath)
-	if err != nil {
+	switch {
+	case err != nil:
 		return fmt.Errorf("can't get file info: %w", err)
-	}
-
-	if fInfo.IsDir() {
+	case fInfo.IsDir():
 		return fmt.Errorf("%w: '%s' is a directory", ErrUnsupportedFile, fromPath)
-	}
-
-	if fInfo.Size() < offset {
+	case fInfo.Size() < offset:
 		return fmt.Errorf(
 			"%w: filesize=%d < offset=%d",
 			ErrOffsetExceedsFileSize, fInfo.Size(), offset,
