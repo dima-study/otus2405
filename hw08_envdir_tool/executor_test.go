a44f350359cc60e0abe7f288d6091c75c28a9307
Dmitry Latin <dim0xff@gmail.com>
2024-07-30 02:30:33 +0300

    test: add prepareCmd tests

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,85 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
 
-func TestRunCmd(t *testing.T) {
-	// Place your code here
+	"github.com/stretchr/testify/require"
+)
+
+func Test_prepareCmd(t *testing.T) {
+	const (
+		toRemove    = "HW08-REMOVE"
+		toRemoveVal = "MUST BE DELETED"
+
+		toOverwrite    = "HW08-OVERWRITE"
+		toOverwriteOld = "MUST BE OVERWRITTEN"
+		toOverwriteNew = "NEW VALUE"
+
+		newVar    = "HW08-NEW"
+		newVarVal = "NEW VAR"
+	)
+
+	os.Setenv(toRemove, toRemoveVal)
+	os.Setenv(toOverwrite, toOverwriteOld)
+
+	t.Run("set vars", func(t *testing.T) {
+		execCmd := prepareCmd(
+			[]string{"test"},
+			Environment{
+				toRemove:    {toRemoveVal, true},
+				toOverwrite: {toOverwriteNew, false},
+				newVar:      {newVarVal, false},
+			},
+		)
+
+		require.Contains(
+			t,
+			execCmd.Env,
+			toOverwrite+"="+toOverwriteOld,
+			"must contains "+toOverwriteOld)
+
+		require.Contains(
+			t,
+			execCmd.Env,
+			toOverwrite+"="+toOverwriteNew,
+			"must contains "+toOverwriteNew)
+
+		require.Contains(
+			t,
+			execCmd.Env,
+			newVar+"="+newVarVal,
+			"must contains "+newVarVal)
+
+		require.NotContains(
+			t,
+			execCmd.Env,
+			toRemove+"="+toRemoveVal,
+			"must not contains "+toRemove)
+	})
+
+	t.Run("set no vars", func(t *testing.T) {
+		execCmd := prepareCmd(
+			[]string{"test"},
+			Environment{},
+		)
+
+		require.NotContains(
+			t,
+			execCmd.Env,
+			toOverwrite+"="+toOverwriteNew,
+			"must not contains "+toOverwriteNew)
+
+		require.NotContains(
+			t,
+			execCmd.Env,
+			newVar+"="+newVarVal,
+			"must not contains "+newVarVal)
+
+		require.Contains(
+			t,
+			execCmd.Env,
+			toRemove+"="+toRemoveVal,
+			"must contains "+toRemove)
+	})
 }
