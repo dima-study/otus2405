9fd6388f94ed7bbac9eeac5fb13fdd126e87e404
Dmitry Latin <dim0xff@gmail.com>
2024-07-30 03:44:50 +0300

    refactor(test): remove unused fields/vars

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -13,14 +13,6 @@ type tableTest struct {
 	dir    string
 	result Environment
 	err    error
-	errIs  error
-	errAs  func(err error) (error, bool)
-}
-
-func genErrAs[T error](target T) func(err error) (error, bool) {
-	return func(err error) (error, bool) {
-		return target, errors.As(err, &target)
-	}
 }
 
 func TestReadDir(t *testing.T) {
@@ -39,22 +31,19 @@ func TestReadDir(t *testing.T) {
 				"UNSET":         EnvValue{"", true},
 				"ZEROES":        EnvValue{"  here\nnew\nline", false},
 			},
-			err:   nil,
-			errAs: nil,
+			err: nil,
 		},
 		{
 			name:   "env dir doesn't exists",
 			dir:    "./testdata/does-not-exist",
 			result: nil,
 			err:    fs.ErrNotExist,
-			errAs:  nil,
 		},
 		{
 			name:   "empty env dir",
 			dir:    "./testdata/empty",
 			result: Environment{},
 			err:    nil,
-			errAs:  nil,
 		},
 	}
 
@@ -62,7 +51,7 @@ func TestReadDir(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			result, err := ReadDir(tt.dir)
 
-			if tt.err != nil || tt.errAs != nil {
+			if tt.err != nil {
 				if err == nil {
 					t.Fatal("error must not be empty, but it is")
 				}
@@ -70,16 +59,8 @@ func TestReadDir(t *testing.T) {
 				if tt.err != nil && !errors.Is(err, tt.err) {
 					t.Errorf("error must be %#v, but got %#v", tt.err, err)
 				}
-
-				if tt.errAs != nil {
-					if target, ok := tt.errAs(err); !ok {
-						t.Errorf("error must be %#v, but got %#v", target, err)
-					}
-				}
-			} else {
-				if err != nil {
-					t.Fatalf("error must be empty, but got %#v", err)
-				}
+			} else if err != nil {
+				t.Fatalf("error must be empty, but got %#v", err)
 			}
 
 			if tt.result != nil && result == nil {
